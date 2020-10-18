00a904b3c4866d7589f412d7c45b76f68d84213b
Sumit M <thesumit67@gmail.com>
2020-10-18 20:51:52 +0530

    Model added

---
+++ Data/ApplicationDBContext.cs
@@ -0,0 +1,7 @@
+namespace TestWebAPI.Data
+{
+    class ApplicationDbContext: DbContext
+    {
+        
+    }
+}
---
+++ Models/NationalPark.cs
@@ -0,0 +1,19 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TestWebAPI.Models
+{
+
+    class NationalPark
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string State { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Established { get; set; }
+    }
+
+}
